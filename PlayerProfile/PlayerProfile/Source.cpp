be9e1b6338b78c24bbf9c172983f8be13f9487fc
Blake Peck <s210561@students.aie.edu.au>
2021-04-26 16:48:03 +1000

    Trying to come with the user profiles that would be used

--- PlayerProfile/PlayerProfile/Source.cpp
+++ PlayerProfile/PlayerProfile/Source.cpp
@@ -8,10 +8,12 @@ int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-
-
-
-
+	UserProfile* pUser[5];
+	pUser[0] = new UserProfile();
+	pUser[1] = new UserProfile();
+	pUser[2] = new UserProfile();
+	pUser[3] = new UserProfile();
+	pUser[4] = new UserProfile();
 	getchar();
 
 	return 0;
