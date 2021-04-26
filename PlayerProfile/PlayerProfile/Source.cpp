255742468afd844ffe1d8057611914f08f83b928
Blake Peck <s210561@students.aie.edu.au>
2021-04-26 15:58:21 +1000

    setting up the files for the assessment

---
+++ PlayerProfile/PlayerProfile/Source.cpp
@@ -0,0 +1,18 @@
+#include <crtdbg.h>
+#include <iostream>
+
+#include "Player.h"
+
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+
+
+
+	getchar();
+
+	return 0;
+}
