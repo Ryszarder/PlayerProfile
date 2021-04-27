3704e9af34563aa9bca81cc9c0353a68f58b59dd
Blake Peck <s210561@students.aie.edu.au>
2021-04-27 10:59:17 +1000

    was using the wrong array

--- PlayerProfile/PlayerProfile/Source.cpp
+++ PlayerProfile/PlayerProfile/Source.cpp
@@ -1,5 +1,6 @@
 #include <crtdbg.h>
 #include <iostream>
+#include <fstream>
 
 #include "UserProfile.h"
 
@@ -8,12 +9,17 @@ int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	UserProfile* pUser[5];
+	/*UserProfile* pUser[5]; //wrong way
 	pUser[0] = new UserProfile();
 	pUser[1] = new UserProfile();
 	pUser[2] = new UserProfile();
 	pUser[3] = new UserProfile();
-	pUser[4] = new UserProfile();
+	pUser[4] = new UserProfile();*/
+
+	int count = 5; //will be set different later to the files created
+
+	UserProfile** pUser = new UserProfile*[count];
+
 	getchar();
 
 	return 0;
