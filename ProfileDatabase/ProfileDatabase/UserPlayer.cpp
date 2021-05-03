4ecdd4a0b92759dffd051131922dae31886b61c0
Blake Peck <s210561@students.aie.edu.au>
2021-05-03 11:56:47 +1000

    filling out the functions in the code

--- ProfileDatabase/ProfileDatabase/UserPlayer.cpp
+++ ProfileDatabase/ProfileDatabase/UserPlayer.cpp
@@ -6,24 +6,24 @@ UserPlayer::UserPlayer()
 
 }
 
-void UserPlayer::SetName()
+void UserPlayer::SetName(char* szText)
 {
-
+	strcpy_s(m_szName, 64, szText);
 }
 
 char* UserPlayer::GetName()
 {
-
+	return m_szName;
 }
 
-void UserPlayer::SetHighScore()
+void UserPlayer::SetHighScore(int nScore)
 {
-
+	m_nScore = nScore;
 }
 
 int UserPlayer::GetHighScore()
 {
-
+	return m_nScore;
 }
 
 
--- ProfileDatabase/ProfileDatabase/UserPlayer.h
+++ ProfileDatabase/ProfileDatabase/UserPlayer.h
@@ -3,13 +3,14 @@ class UserPlayer
 {
 public:
 	UserPlayer();
-	void SetName();
+	void SetName(char* szText);
 	char* GetName();
 
-	void SetHighScore();
+	void SetHighScore(int nScore);
 	int GetHighScore();
 
 private:
-
+	char m_szName[64];
+	int m_nScore;
 };
 
