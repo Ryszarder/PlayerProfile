2ff242e5362755b4df1f0d18517965d76f082edd
Blake Peck <s210561@students.aie.edu.au>
2021-05-03 10:45:38 +1000

    sure

--- PlayerProfile/PlayerProfile/Source.cpp
+++ PlayerProfile/PlayerProfile/Source.cpp
@@ -6,28 +6,28 @@
 #include "DataManage.h"
 
 
-void Profile(DataManage* data)
-{
-	int count = 3;
-	UserProfile** pUser = new UserProfile*[count];
-	pUser[0] = new UserProfile();
-	pUser[1] = new UserProfile();
-	pUser[2] = new UserProfile();
-
-	char name[64] ="testsubject 0";
-	char name1[64] ="testsubject 1";
-	char name2[64] ="testsubject 2";
-
-	pUser[0]->SetName(name);
-	pUser[1]->SetName(name1);
-	pUser[2]->SetName(name2);
-
-	pUser[0]->SetHighScore(100);
-	pUser[1]->SetHighScore(50);
-	pUser[2]->SetHighScore(10);
-
-	data->WriteFile(pUser);
-}
+//void Profile(DataManage* data)
+//{
+//	int count = 3;
+//	UserProfile** pUser = new UserProfile*[count];
+//	pUser[0] = new UserProfile();
+//	pUser[1] = new UserProfile();
+//	pUser[2] = new UserProfile();
+//
+//	char name[64] ="testsubject 0";
+//	char name1[64] ="testsubject 1";
+//	char name2[64] ="testsubject 2";
+//
+//	pUser[0]->SetName(name);
+//	pUser[1]->SetName(name1);
+//	pUser[2]->SetName(name2);
+//
+//	pUser[0]->SetHighScore(100);
+//	pUser[1]->SetHighScore(50);
+//	pUser[2]->SetHighScore(10);
+//
+//	data->WriteFile(pUser);
+//}
 
 int main()
 {
@@ -35,7 +35,7 @@ int main()
 
 	DataManage* data = new DataManage();
 	
-	Profile(data);
+	/*Profile(data);*/
 
 	//UserProfile* pUser = data->ReadFile(*data, count);
 
